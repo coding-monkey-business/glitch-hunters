fec5b29884b098ab19b31ecae24b9039c9a6697a
p1100i <p1100i.dev@gmail.com>
2016-08-24 12:35:24 +0200

    removes unnecessary mock markup from test

--- test/a.spec.js
+++ test/a.spec.js
@@ -1,11 +1,6 @@
 var
   test = window.test;
 
-//
-// Ensure proper markup for src/a.js
-//
-document.body.innerHTML = '<canvas id="c"></canvas>';
-
 describe('.getId', function () {
   it('should return newer ids on every call', function () {
     expect(test.getId()).toBe(1);
