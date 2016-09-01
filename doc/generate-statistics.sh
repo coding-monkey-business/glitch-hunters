677514989571f4b7abaf0d5eb796c907f57c0813
p1100i <p1100i.dev@gmail.com>
2016-09-01 09:56:22 +0200

    moves statistics / generator into doc

--- doc/generate-statistics.sh
+++ doc/generate-statistics.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 
+BASEDIR=$(dirname "$0")
+cd "$BASEDIR/.."
+
 if [ -n "$(git status --porcelain)" ]; then
   echo "There are changes in your working tree, STOPPING.";
   exit 1;
 else
-  echo "I detected no changes in your working tree, in 5 seconds, imma start doing statistics by messing up your git, CTRL-C me, if you want this to stop.";
+  echo -e "I detected no changes in your working tree.\nIn 5 seconds, Imma start doing statistics by messing up your git.\nCTRL-C me, if you want this to stop.";
   sleep 5
 fi
 
 log=$(git log --oneline)
-# log=$(git log --oneline | tac)
 
 function grunt_size_in_git_commit()
 {
+  echo "DIRTY - git is dirty, DON'T STOP ME NOW!"
+
   git checkout $1 src Gruntfile.js
+
+  # Some sed magic to get right info into the .csv file
   gruntline=$(./grunt build:advzip | tail -n 3 | head -n 1)
-  # Remove color escapes with sed, if needed.
-  # size=$(echo $gruntline | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//" )
-  echo $gruntline
   size=$(echo $gruntline | sed "s/[^0-9]*//")
   size=$(echo $size | sed "s/[^(]*(\([0-9]*\).*/\1/")
   out=$(echo $gruntline | cut -c1-50)
 
   echo "$1; $size; $2; $out"
-  echo "$1; $size; $2; $out;" >> stats.csv
+  echo "$1; $size; $2; $out;" >> doc/stats.csv
   git reset -q HEAD src Gruntfile.js
   git checkout src Gruntfile.js
   git clean -qf src
+
+  echo "CLEAN - git is clean again for 1 sec, CTRL-C me now to stop w/ clean repo state."
+  sleep 1
 }
 
-echo "COMMIT; BYTESIZE; OUTPUT;" >> stats.csv
+echo "COMMIT; BYTESIZE; OUTPUT;" >> doc/stats.csv
 
 while read -r first line; do
   grunt_size_in_git_commit "$first" "$line"
